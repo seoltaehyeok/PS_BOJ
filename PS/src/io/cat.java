847914321a0b7fe6c30010685a3433523fa4ff08
seoltaehyeok <ajaj312216@gmail.com>
2021-12-04 15:51:15 +0900

    no.10171

---
+++ PS/src/io/cat.java
@@ -0,0 +1,12 @@
+package io;
+
+public class cat {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");  
+	}
+
+}
