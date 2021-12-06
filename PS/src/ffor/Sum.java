58486223e43f8fd2d853c07a0a7bcd36dd44caa9
seoltaehyeok <ajaj312216@gmail.com>
2021-12-06 15:49:58 +0900

    no.8393

---
+++ PS/src/ffor/Sum.java
@@ -0,0 +1,18 @@
+package ffor;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int sum=0;
+		for(int i=1;i<=n; i++) {
+			sum+=i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
