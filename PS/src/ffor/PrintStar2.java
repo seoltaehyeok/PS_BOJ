e69a34cc05cd3dd1c60ebe0d4efc9a9ae6e6225f
seoltaehyeok <ajaj312216@gmail.com>
2021-12-06 15:57:25 +0900

    no.2439

---
+++ PS/src/ffor/PrintStar2.java
@@ -0,0 +1,29 @@
+package ffor;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class PrintStar2 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int Case = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<Case; i++) {
+			for(int j=i; j<Case-1; j++) {
+				bw.write(" ");
+			}
+			for(int k=0; k<i+1; k++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
