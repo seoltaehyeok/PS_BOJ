433e6f051618f6c9760c523d42d45a931179d60c
taehyeok <ajaj3122@naver.com>
2022-01-02 21:38:47 +0900

    no.5622

--- PS/src/string/Dial.java
+++ PS/src/string/Dial.java
@@ -8,34 +8,27 @@ public class Dial {
 		Scanner sc = new Scanner(System.in);
 		String DialText = sc.nextLine();
 		char[] ch = new char[DialText.length()];
-		
+
 		int time = 0;
-		
-		for(int i=0; i<DialText.length(); i++) {
+
+		for (int i = 0; i < DialText.length(); i++) {
 			ch[i] = DialText.charAt(i);
-			if(ch[i]-'A'<3) {
-				time+=3;
-			}
-			else if(ch[i]-'A'<6) {
-				time+=4;
-			}
-			else if(ch[i]-'A'<9) {
-				time+=5;
-			}
-			else if(ch[i]-'A'<12) {
-				time+=6;
-			}
-			else if(ch[i]-'A'<15) {
-				time+=7;
-			}
-			else if(ch[i]-'A'<19) {
-				time+=8;
-			}
-			else if(ch[i]-'A'<22) {
-				time+=9;
-			}
-			else {
-				time+=10;
+			if (ch[i] - 'A' < 3) {
+				time += 3;
+			} else if (ch[i] - 'A' < 6) {
+				time += 4;
+			} else if (ch[i] - 'A' < 9) {
+				time += 5;
+			} else if (ch[i] - 'A' < 12) {
+				time += 6;
+			} else if (ch[i] - 'A' < 15) {
+				time += 7;
+			} else if (ch[i] - 'A' < 19) {
+				time += 8;
+			} else if (ch[i] - 'A' < 22) {
+				time += 9;
+			} else {
+				time += 10;
 			}
 		}
 		System.out.println(time);
